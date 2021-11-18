ed59a92f418407e0309237d0142ae188efc31906
jjimini98 <jjimini98@naver.com>
2021-11-18 16:57:38 +0900

    1118 모각코 7일차 과제

--- cogs/example.py
+++ cogs/example.py
@@ -2,7 +2,7 @@
 # 명령어를 여러개를 모아 하나의 클래스를 구성
 
 from discord.ext import commands
-
+import discord
 # Example 클래스는 commands.Cog 클래스를 상속 
 # commands.Cog는 모든 Cog 클래스가 상속해야하는 기반 클래스 .
 class Example(commands.Cog): 
@@ -17,7 +17,32 @@ class Example(commands.Cog):
     @commands.command(name= "ping")
     async def _ping(self,ctx):
         await ctx.send("pong!")
+
+
+    @commands.command(name = "임베드")
+    async def embed(self, ctx):
+        embed=discord.Embed(
+            title="Embed의 제목입니다.",
+            description="임베드 설명입니다.", 
+            color=discord.Color.blue()
+        )
+        embed.set_author(
+            name="Jimin", 
+            url="https://cafe.naver.com/codeuniv", 
+            icon_url="https://source.unsplash.com/random")
+
+
+        embed.add_field(
+            name="Field 1 Title", 
+            value="This is the value for field 1. This is NOT an inline field.", 
+            inline=False) 
+        embed.add_field(name="Field 2 Title", value="It is inline with Field 3", inline=True)
+        embed.add_field(name="Field 3 Title", value="It is inline with Field 2", inline=True)
+
+        embed.set_thumbnail(url="https://source.unsplash.com/random")
     
+        await ctx.send(embed = embed)
+        
 # 함수 이름은 반드시 setup이어야 하고 인자로 봇 객체 하나만 받아야 함. 
 def setup(client):
     client.add_cog(Example(client))
--- cogs/quiz.py
+++ cogs/quiz.py
@@ -1,3 +1,5 @@
+import discord
+from discord import embeds
 from discord.ext import commands
 import csv
 import random 
@@ -8,8 +10,8 @@ class Quiz(commands.Cog):
     def __init__(self, client):
         self.client = client
         self.quizDict = {}
-        self.scoreDict = {} #이것도 여기서 관리하는게 맞나.. 
-        self.score = 0 #이걸 여기서 관리하는게 맞나.. 
+        # self.scoreDict = {} #이것도 여기서 관리하는게 맞나.. 
+        # self.score = 0 #이걸 여기서 관리하는게 맞나.. 
         with open("C:/Users/Jimin/PycharmProjects/discord/cogs/data/quiz.csv", 'r', encoding='utf-8') as f:
             reader = csv.reader(f)
             for row in reader:
@@ -24,34 +26,50 @@ class Quiz(commands.Cog):
         problemList = list(self.quizDict.keys()) #여기서 딕셔너리의 key는 문제임 
         problem = random.choice(problemList) #문제 리스트에서 하나 랜덤으로 추출 
         answer = self.quizDict[problem] #문제에 해당하는 value가 answer 변수에 들어감 
-        await ctx.send(problem) #문제 출력 
-
+        # await ctx.send(problem) #문제 출력 
+        embed = discord.Embed(title = '퀴즈', description = problem, color = discord.Color.blue())
+        await ctx.send(embed=embed)
+        
         def checkAnswer(message):
-            # message.channel == ctx.channel 입력한 정답의 채널과 명령어가 입력된 채널이 같은지 
-            # answer in message.content : 메세지의 내용에 정답이 들어있는지? 
             if message.channel == ctx.channel and answer in message.content:
                 return True
             else:
                 return False
 
         try:
-            # wait_for는 세 개의 인자를 받음 (1. 이벤트: 어떤 이벤트가 발생하기를 기다리는 함수, 여기서는 message를 기다리는 함수 ,
-            # 2. 이벤트 유효성 , check : 여기서 유효한 메세지 정답을 기다림. 이 때 check 인자에 들어가는 checkAnswer 함수 -> 이게 True일때 통과) 
-            await self.client.wait_for("message", timeout= 10.0, check = checkAnswer)
-            await ctx.send("정답이에요!")
+            message = await self.client.wait_for("message", timeout = 10.0, check = checkAnswer)
+            name = message.author.name
+            embed = discord.Embed(title = '', description = f'{name} 님, 정답이에요 !', color = discord.Color.blue())
+            await ctx.send(embed=embed)
             self.score +=1 
             self.scoreDict[ctx.author.name] = self.score
             print(self.scoreDict)
 
         except asyncio.TimeoutError:
-            await ctx.send(f"땡! 시간초과에요! 정답은 {answer}이에요!")
+            embed = discord.Embed(title = '', description = f'땡! 시간초과입니다~ 정답은 {answer}이에요!', color = discord.Color.red())
+            await ctx.send(embed=embed)
             self.score += 0 
             self.scoreDict[ctx.author.name] = self.score
             print(self.scoreDict)
 
         with open("C:/Users/Jimin/PycharmProjects/discord/cogs/data/score.json","w", encoding= 'utf-8') as s: #에러부분 
-             json.dump(self.scoreDict, s, ensure_ascii=False)
+            json.dump(self.scoreDict, s, ensure_ascii=False)
         print("===========Modified JSON File===========")
+
+
+    @commands.command(name="퀴즈랭킹")
+    async def ranking_quiz(self,ctx):
+        embed = discord.Embed(title = '전체 퀴즈 랭킹', description = "전체 퀴즈 랭킹입니다. \n 한 문제 맞출 때마다 1점이 증가해요! ", color = discord.Color.blue())
+        with open("C:/Users/Jimin/PycharmProjects/discord/cogs/data/score.json","r", encoding= 'utf-8') as s:
+            data = json.load(s)
+    
+        sdict = sorted(dict.items(data)) #점수를 내림차순으로 정렬 
+        idx = 1
+        for name,score in sdict: 
+            mod_name = str(idx)+"."+name
+            embed.add_field(name=mod_name,value=score,inline=False)
+            idx += 1
+        await ctx.send(embed=embed)
 
 
     @commands.command(name="퀴즈종료")
